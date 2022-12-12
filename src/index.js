10d8534aedd46c8c7c7f91849ab961e795dd753b
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-12-13 02:04:31 +0900

    ⚡️ made a reducer to incerase and decrease counter

--- src/index.js
+++ src/index.js
@@ -5,31 +5,40 @@ import App from "./App";
 import { createStore } from "redux";
 //createStore に横線が入っているのは非推奨になっているからであって、使えないと言う意味ではない。
 
-//storeを作る。
-let store = createStore(counterReducer);
 
-//actionsを作る。increment,decrementという名前のactionを作る。
-//typeと書いてあるところは意味は特になく、ただの名前。定義しているだけ。
-const incriment = () => {
-  return{
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return{
-    type: "DECREMENT",
-  };
-};
 //reducerを作る。アクションと前の状態を組み合わせて新しい状態（state）を作る。
 const counterReducer = (state = 0, action) => {
-  switch(action.type){
+  switch (action.type) {
     case "INCREMENT":
       return state + 1;
     case "DECREMENT":
       return state - 1;
   }
 };
+
+//storeを作る。
+let store = createStore(counterReducer);
+
+//actionsを作る。increment,decrementという名前のactionを作る。
+//typeと書いてあるところは意味は特になく、ただの名前。定義しているだけ。
+const incriment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+//コンソールに新しいstateを表示する。
+store.subscribe(() => console.log(store.getState()));
+
+//dispatchを使ってactionを実行する。
+store.dispatch(incriment());
+store.dispatch(incriment());
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
