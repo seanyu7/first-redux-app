8644f2254d792c5af745fe98ff70096e6c598bee
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-10 22:44:55 +0900

    ⚡️ made use of provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
