a73edaae494bfa7e7e8c6c74390a02797ac7807f
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-10 21:53:29 +0900

    ⚡️  set a login function for a reducer

---
+++ src/reducers/isLoggin.js
@@ -0,0 +1,12 @@
+const isLogginReducer = (state = false, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return !state;
+    default:
+      return state;
+  }
+};
+//ここで書かれているものは、storeの中のreducerにどのような動きをして欲しいかを書いているだけ。
+//初期値を設定して、そのあとにaction.typeによってどのような動きをするかを書いている。
+
+export default isLogginReducer;
