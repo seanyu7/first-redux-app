ad054f09be7f0ddce278c0bc3416045297d0a649
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-11 22:03:11 +0900

    💄 connecting UI with redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App">
-    <h1>Hello Redux</h1>
-  </div>;
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+
+  return (
+    <div className="App">
+      <h1>Hello Redux</h1>
+      <h3>Counter {counter}</h3>
+      {isLogin ? <h3>logged in</h3> : <h3>Please Login</h3>}
+    </div>
+  );
 }
 
 export default App;
