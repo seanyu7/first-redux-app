8bfa2dcc587b9d0ee83d7c28c74cadae1d1321c3
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-11 22:11:32 +0900

    ⚡️ made a action to use redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
+import {increment, decrement} from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>Counter {counter}</h3>
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button onClick={()=> dispatch(decrement())}>-</button>
       {isLogin ? <h3>logged in</h3> : <h3>Please Login</h3>}
     </div>
   );
